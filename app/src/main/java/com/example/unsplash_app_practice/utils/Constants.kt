fa4ad637fdeec85ad6679c406427d9e01258d5e1
Lee SeokCheol <90691610+SeokCheol-Lee@users.noreply.github.com>
2023-04-05 11:28:16 +0900

    Update Constants.kt

--- app/src/main/java/com/example/unsplash_app_practice/utils/Constants.kt
+++ app/src/main/java/com/example/unsplash_app_practice/utils/Constants.kt
@@ -15,7 +15,7 @@ enum class RESPONSE_STATE{
 }
 object API{
     const val BASE_URL : String = "https://api.unsplash.com/"
-    const val CLIENT_ID : String = "kbVgM7oHm87cjCoFwEkL0nkb41YqmbLqBeeG_kCqEJ8"
+    const val CLIENT_ID : String = "인증키"
     const val SEARCH_PHOTO : String = "search/photos"
     const val SEARCH_USERS : String = "search/users"
 }
